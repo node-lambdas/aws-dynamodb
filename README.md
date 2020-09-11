e5f18be1a985f38899058118f34214b3f54aec0d
Darlan Alves <me@darlanalv.es>
2020-09-11 23:30:09 +0200

    feat: add first versions

---
+++ README.md
@@ -0,0 +1,5 @@
+# aws-dynamodb
+
+## Required credentials
+
+`access-key-id` and `access-key` are required
--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-import { lambda } from '@node-lambdas/core';
+import { lambda, Format } from '@node-lambdas/core';
+import aws from 'aws-sdk';
 
+function getDynamo(input) {
+  const accessKeyId = input.credentials['access-key-id'];
+  const secretAccessKey = input.credentials['access-key-secret'];
+  return new aws.DynamoDB({ credentials: { accessKeyId, secretAccessKey } });
+}
+
+const credentials = ['access-key-id', 'access-key-secret'];
 const configutation = {
   version: 2,
   actions: {
-    echo: {
+    delete: {
+      credentials,
+      input: Format.Json,
+      handler: (input, output) => {
+        const dynamo = getDynamo(input);
+        dynamo.deleteItem(input.body, () => output.send(202));
+      },
+    },
+    get: {
       default: true,
-      handler: (input, output) => input.pipe(output),
-    }
-  }
+      credentials,
+      input: Format.Json,
+      handler: (input, output) => {
+        const dynamo = getDynamo(input);
+        dynamo.scan(input.body, (data) => output.send(200, data));
+      },
+    },
+    create: {
+      credentials,
+      input: Format.Json,
+      handler: (input, output) => {
+        const dynamo = getDynamo(input);
+        dynamo.putItem(input.body, () => output.send(201));
+      },
+    },
+    update: {
+      credentials,
+      input: Format.Json,
+      handler: (input, output) => {
+        const dynamo = getDynamo(input);
+        dynamo.updateItem(input.body, () => output.send(200));
+      },
+    },
+  },
 };
 
 lambda(configutation);
