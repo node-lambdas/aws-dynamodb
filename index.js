793e4815c56b5d10515ee2c8629b7b217df3f308
Darlan Alves <me@darlanalv.es>
2020-09-13 01:44:43 +0200

    fix: read region from credentials

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-import { lambda, Format } from '@node-lambdas/core';
+import { Format, lambda } from '@node-lambdas/core';
 import aws from 'aws-sdk';
 
 function getDynamo(input) {
   const accessKeyId = input.credentials['access-key-id'];
   const secretAccessKey = input.credentials['access-key-secret'];
-  return new aws.DynamoDB({ credentials: { accessKeyId, secretAccessKey } });
+  const region = input.credentials['region'];
+
+  return new aws.DynamoDB({
+    region,
+    credentials: new aws.Credentials(accessKeyId, secretAccessKey),
+  });
 }
 
-const credentials = ['access-key-id', 'access-key-secret'];
-const configutation = {
+function executeAction(action, input, output, statusCode) {
+  getDynamo(input)[action](input.body, (error, data) => {
+    if (error) {
+      return output.reject(error);
+    }
+
+    output.send(statusCode, data);
+  });
+}
+
+const credentials = ['access-key-id', 'access-key-secret', 'region'];
+
+lambda({
   version: 2,
   actions: {
     delete: {
       credentials,
       input: Format.Json,
-      handler: (input, output) => {
-        const dynamo = getDynamo(input);
-        dynamo.deleteItem(input.body, () => output.send(202));
-      },
+      output: Format.Json,
+      handler: (input, output) => executeAction('deleteItem', input, output, 202),
     },
     get: {
       default: true,
       credentials,
       input: Format.Json,
-      handler: (input, output) => {
-        const dynamo = getDynamo(input);
-        dynamo.scan(input.body, (data) => output.send(200, data));
-      },
+      output: Format.Json,
+      handler: (input, output) => executeAction('scan', input, output, 200),
     },
     create: {
       credentials,
       input: Format.Json,
-      handler: (input, output) => {
-        const dynamo = getDynamo(input);
-        dynamo.putItem(input.body, () => output.send(201));
-      },
+      output: Format.Json,
+      handler: (input, output) => executeAction('putItem', input, output, 201),
     },
     update: {
       credentials,
       input: Format.Json,
-      handler: (input, output) => {
-        const dynamo = getDynamo(input);
-        dynamo.updateItem(input.body, () => output.send(200));
-      },
+      output: Format.Json,
+      handler: (input, output) => executeAction('updateItem', input, output, 200),
     },
   },
-};
-
-lambda(configutation);
+});
